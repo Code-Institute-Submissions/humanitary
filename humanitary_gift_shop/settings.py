63f31886535065efbe21cdebc288f50d107896f0
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-14 20:10:34 +0200

    Add if else statement to determine wether database is on heroku or on local machine

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -135,16 +135,20 @@ WSGI_APPLICATION = 'humanitary_gift_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = ''
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600, ssl_require=True)
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
