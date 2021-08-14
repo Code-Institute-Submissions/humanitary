ad22a2bc884beb07e10853ba9ed9e36bf2dc7f8f
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-14 18:55:58 +0200

    change debug mode to False

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['humanitary.herokuapp.com', '127.0.0.1']
 
