0a1635df5121046c08b84175f547b26f9a12d23e
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-14 19:00:04 +0200

    Add secret key

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -13,10 +13,11 @@ from pathlib import Path
 import os
 import dj_database_url
 
+SECRET_KEY = 'django-insecure-l3%tldm@6bc^1zv*-m6&!zyh6c&mgga^wwwg@2-(ntr77*$^^5'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = 'django-insecure-l3%tldm@6bc^1zv*-m6&!zyh6c&mgga^wwwg@2-(ntr77*$^^5'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
