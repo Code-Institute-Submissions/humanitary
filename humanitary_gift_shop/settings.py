347c27debb9d97582b8cbede05fe70b6d3924ec8
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-14 18:57:50 +0200

    Remove secret key from settings.py

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from .secretstuff import SECRET_KEY
 from pathlib import Path
 import os
 import dj_database_url
@@ -18,7 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = SECRET_KEY
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
