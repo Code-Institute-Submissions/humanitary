777491bdb8f8116db63761acab5e6170251979d2
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-14 16:16:32 +0200

    Add heroku and localserver to allowed hosts and set debug to false

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l3%tldm@6bc^1zv*-m6&!zyh6c&mgga^wwwg@2-(ntr77*$^^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['humanitary.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
