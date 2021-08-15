7e0f971f6540b5ee035f695c5e97abb2a0090fdc
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-15 20:52:29 +0200

    Add boto file with host information

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -202,6 +202,7 @@ if 'USE_AWS' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_HOST = 's3.eu-north-1.amazonaws.com'
 
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
