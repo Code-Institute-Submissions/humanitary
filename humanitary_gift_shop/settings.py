08e57a0cd496be10730196169d76aab51f5cb3f4
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-15 19:23:28 +0200

    Deploy project using AWS S3 services for static files

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import custom_storages
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 print(SECRET_KEY)
