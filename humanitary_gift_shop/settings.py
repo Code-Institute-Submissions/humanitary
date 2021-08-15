d24570e9011bc17ba347d9f8a71111f2bc8406ba
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-15 21:25:48 +0200

    Test deploy with new storage backend

--- humanitary_gift_shop/settings.py
+++ humanitary_gift_shop/settings.py
@@ -204,7 +204,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_HOST = 's3.eu-north-1.amazonaws.com'
 
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
