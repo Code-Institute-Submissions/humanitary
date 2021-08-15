83b8f315d8904da2b61013dfff18800f7de98180
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-15 19:45:25 +0200

    Deploy project using AWS S3 services for static files

---
+++ staticfiles/js/store.d56cf1354085.js
@@ -0,0 +1,67 @@
+const updateCart = document.getElementsByClassName("update-cart");
+
+// Takes information about the product the user has clicked. Used code snippets from Denis Ivy's tutorial on
+// django E-commerce, although somewhat modified to fit my own needs.
+
+for (let i = 0; i < updateCart.length; i++) {
+  updateCart[i].addEventListener("click", function () {
+    let productId = this.dataset.product;
+    let action = this.dataset.action;
+    console.log("productId:", productId, "action:", action);
+    if (user === "AnonymousUser") {
+      guestUserItem(productId, action);
+    } else {
+      updateOrder(productId, action);
+    }
+  });
+}
+
+// Updates and adds cookie items to the guest user cart
+function guestUserItem(productId, action) {
+  console.log("User is not logged in");
+
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("Remove item");
+      delete cart[productId];
+    }
+  }
+  if (action == "delete") {
+    delete cart[productId];
+  }
+  console.log("Cart", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
+  location.reload();
+}
+
+// Updates the order with the clicked items if the user is logged in
+updateOrder = function (productId, action) {
+  console.log("User is authenticated, sending data...");
+
+  let url = "/cart/update_cart/";
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data:", data);
+      location.reload();
+    });
+};
