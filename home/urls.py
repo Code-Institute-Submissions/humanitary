80803199994478b175ded48266ebb6289318c1ab
Jonny Björnhager <jonny.bjornhager@live.se>
2021-08-17 13:20:08 +0200

    Add robots.txt

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path("robots.txt", views.robots_txt),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
-from accounts.models import Order
 
 
 @xframe_options_exempt
@@ -13,3 +14,14 @@ def index(request):
 def error_404(request, exception):
     data = {}
     return render(request, 'home/404.html', data)
+
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-Agent: *",
+        "Disallow: /checkout/thank-you.html/",
+        "Disallow: /accounts/login.html/",
+        "Disallow: /accounts/logout.html/"
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
